b538fb68273e5b6ec204239def71931fa1da1ea2
JessicaThornsby <jessica.thornsby@gmail.com>
2017-05-18 15:35:47 +0100

    First commit

---
+++ app/src/main/java/com/jessicathornsby/textinput/MainActivity.java
@@ -0,0 +1,20 @@
+package com.jessicathornsby.textinput;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.support.design.widget.TextInputLayout;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }}
